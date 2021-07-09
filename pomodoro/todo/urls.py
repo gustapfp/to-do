4409ce34b3ef57eb2212c1c1bdb6aedaa68da85e
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2021-07-09 02:26:33 -0300

    delete and edit tasks completed

--- pomodoro/todo/urls.py
+++ pomodoro/todo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('update-task/<str:pk>/', views.updateTasks, name='update-task'),
+    path('delete/<str:pk>/', views.deleteTasks, name='delete')
 ]
--- pomodoro/todo/views.py
+++ pomodoro/todo/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
-from django.http import request
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import *
+from .forms import *
 
 def index(request):
-    #return HttpResponse("Hello!")
-    return render(request, 'index.html')
+    task = Tasks.objects.all()
+    form = TasksForm()
+
+    if request.method == 'POST':
+        form = TasksForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    context = {
+        'tasks': task,
+        'form': form
+    }
+    return render(request, 'index.html', context)
+
+def updateTasks(request, pk):
+    task = Tasks.objects.get(id=pk)
+    form = TasksForm(instance=task)
+
+    if request.method == 'POST':
+        form = TasksForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {
+        'form': form
+    }
+    return render(request, 'update_tasks.html', context)
+
+def deleteTasks(request, pk):
+    item = Tasks.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect("/")
+
+    context = {
+        'item':item
+    }
+    return render(request, 'delete.html', context)
